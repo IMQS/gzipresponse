9505fbb892be75de997b1d5f4edb4ffbf0453032
Ben Harper <rogojin@gmail.com>
2016-07-22 08:36:04 +0200

    Add gzipresponse.go

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # gzipresponse
-gzip HTTP response writer for Go
+gzip HTTP response writer for Go.
+
+This package contains just one function called `Write`, which you use to write your data into an `http.ResponseWriter`.
+
+We created this as an alternative to http://github.com/NYTimes/gziphandler, because it seems impossible to use that handler in combination with http://github.com/julienschmidt/httprouter.
---
+++ gzipresponse.go
@@ -0,0 +1,30 @@
+// Package gzipresponse is a tiny helper to gzip an HTTP response
+package gzipresponse
+
+import (
+	"compress/gzip"
+	"net/http"
+	"strings"
+)
+
+// Use this as a replacement for w.Write()
+func Write(w http.ResponseWriter, r *http.Request, data []byte) (int, error) {
+	if strings.Index(r.Header.Get("Accept-Encoding"), "gzip") == -1 {
+		return w.Write(data)
+	}
+
+	if _, ok := w.Header()["Content-Type"]; !ok {
+		// If content type is not set, infer it from the uncompressed body.
+		// The net/http package normally does this, but that wasn't written to
+		// work on compressed data.
+		w.Header().Set("Content-Type", http.DetectContentType(data))
+	}
+
+	zipper := gzip.NewWriter(w)
+	bytes, err := zipper.Write(data)
+	errClose := zipper.Close()
+	if err != nil {
+		return bytes, err
+	}
+	return bytes, errClose
+}
